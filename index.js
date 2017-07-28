0523ab01262c1c91a11e950506e05f63247485c1
aerow49 <madscience9@gmail.com>
2017-07-27 23:25:32 -0400

    Graphics redesign
    Improved the overall design.

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-var  c = [];
+var c = [];
 var ol = [];
 
-function addPlayer(){
+function addPlayer() {
     var n = document.getElementById("n");
     c[c.length] = n.value;
     //console.log(c);
     var t = document.getElementById("p");
-    var row = t.insertRow(c.length-1);
+    if (c.length % 2 == 0)
+        t = document.getElementById("p2");
+    var row = t.insertRow();
     var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
     var button = document.createElement("button");
-    button.innerHTML = "X  "+n.value;
-    button.addEventListener ("click", function() {
-        var del = this.innerHTML.slice(3,this.innerHTML.length);
-        c.splice(c.indexOf(del),1);
-        ol.splice(ol.indexOf(del),1);
+    button.innerHTML = "X  " + n.value;
+    button.id = "rems";
+    button.addEventListener("click", function() {
+        var del = this.innerHTML.slice(3, this.innerHTML.length);
+        c.splice(c.indexOf(del), 1);
+        ol.splice(ol.indexOf(del), 1);
         console.log(c);
         //this.parentElement.parentElement.parentElement.remove();
         this.parentElement.parentElement.remove();
@@ -22,21 +24,23 @@ function addPlayer(){
         this.remove();
     });
     cell1.appendChild(button);
-    n.value = ""; 
+    n.value = "";
 }
 
-function randomize(){
+function randomize() {
     ol = c.slice();
-    ol.sort(function(a,b){return 0.5-Math.random()});
+    ol.sort(function(a, b) { return 0.5 - Math.random() });
 }
 
-function next(){
-    for(var i=1;i<5;i++){
-        if(ol.length <= 0)
-            randomize();
-        //console.log(ol.length);
-        var p = document.getElementById(""+i);
-        p.innerHTML = ol[ol.length-1];
-        ol.pop();
+function next() {
+    if (c.length > 0) {
+        for (var i = 1; i < 5; i++) {
+            if (ol.length <= 0)
+                randomize();
+            //console.log(ol.length);
+            var p = document.getElementById("" + i);
+            p.innerHTML = ol[ol.length - 1];
+            ol.pop();
+        }
     }
 }
