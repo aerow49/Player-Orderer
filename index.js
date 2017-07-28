e48a63068f3e6fd8459c9c538b5a8c84a3dd1313
aerow49 <madscience9@gmail.com>
2017-07-27 23:30:32 -0400

    Add Empty Names
    added a check to not allow empty names to be added

--- index.js
+++ index.js
@@ -3,28 +3,30 @@ var ol = [];
 
 function addPlayer() {
     var n = document.getElementById("n");
-    c[c.length] = n.value;
-    //console.log(c);
-    var t = document.getElementById("p");
-    if (c.length % 2 == 0)
-        t = document.getElementById("p2");
-    var row = t.insertRow();
-    var cell1 = row.insertCell(0);
-    var button = document.createElement("button");
-    button.innerHTML = "X  " + n.value;
-    button.id = "rems";
-    button.addEventListener("click", function() {
-        var del = this.innerHTML.slice(3, this.innerHTML.length);
-        c.splice(c.indexOf(del), 1);
-        ol.splice(ol.indexOf(del), 1);
-        console.log(c);
-        //this.parentElement.parentElement.parentElement.remove();
-        this.parentElement.parentElement.remove();
-        this.parentElement.remove();
-        this.remove();
-    });
-    cell1.appendChild(button);
-    n.value = "";
+    if (n.value != "") {
+        c[c.length] = n.value;
+        //console.log(c);
+        var t = document.getElementById("p");
+        if (c.length % 2 == 0)
+            t = document.getElementById("p2");
+        var row = t.insertRow();
+        var cell1 = row.insertCell(0);
+        var button = document.createElement("button");
+        button.innerHTML = "X  " + n.value;
+        button.id = "rems";
+        button.addEventListener("click", function() {
+            var del = this.innerHTML.slice(3, this.innerHTML.length);
+            c.splice(c.indexOf(del), 1);
+            ol.splice(ol.indexOf(del), 1);
+            console.log(c);
+            //this.parentElement.parentElement.parentElement.remove();
+            this.parentElement.parentElement.remove();
+            this.parentElement.remove();
+            this.remove();
+        });
+        cell1.appendChild(button);
+        n.value = "";
+    }
 }
 
 function randomize() {
